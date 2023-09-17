c01110e537570c7255caf15357968fe02e6311a0
Ivan <korablev2002@yandex.ru>
2023-09-17 13:14:35 +0400

    Added Submit Post button enabled check

--- app/src/main/java/com/example/glazovnetadminapp/presentation/posts/PostsScreen.kt
+++ app/src/main/java/com/example/glazovnetadminapp/presentation/posts/PostsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.glazovnetadminapp.presentation.posts.addPost.AddPostScreen
+import com.example.glazovnetadminapp.presentation.posts.postsList.PostsScrollableList
 
 @Composable
 fun PostsScreen(
