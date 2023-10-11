774364b7a6f9893298e3a198901d39b7533c5216
Ivan <korablev2002@yandex.ru>
2023-10-11 18:30:31 +0500

    Menu remake

--- app/src/main/java/com/example/glazovnetadminapp/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/glazovnetadminapp/presentation/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.glazovnetadminapp.presentation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,13 +10,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.glazovnetadminapp.R
 import com.example.glazovnetadminapp.presentation.destinations.AddPostScreenDestination
 import com.example.glazovnetadminapp.presentation.destinations.PostsScreenDestination
 import com.example.glazovnetadminapp.presentation.destinations.TariffsScreenDestination
@@ -22,79 +32,104 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph(start = true)
 @Destination
 @Composable
 fun HomeScreen(
     navigator: DestinationsNavigator
 ) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize()
-    ) {
-        Text(
-            text = "Api.Glazov.Net",
-            style = MaterialTheme.typography.displayMedium
-        )
-        Text(
-            text = "Admin App",
-            style = MaterialTheme.typography.displaySmall
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Divider(modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(
-            text = "Posts",
-            style = MaterialTheme.typography.displaySmall
-        )
-        Card(
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                }
+            )
+        }
+    ) { values ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .padding(values)
         ) {
-            OutlinedButton(
+            Text(
+                text = stringResource(id = R.string.menu_posts_title_text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            MenuButton(
+                text = stringResource(id = R.string.app_posts_screen_name),
                 onClick = {
                     navigator.navigate(PostsScreenDestination, onlyIfResumed = true)
-                },
-                modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = "All posts"
-                )
-            }
-            OutlinedButton(
+                }
+            )
+            MenuButton(
+                text = stringResource(id = R.string.app_add_post_screen_name),
                 onClick = {
                     navigator.navigate(AddPostScreenDestination, onlyIfResumed = true)
-                },
+                }
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.menu_tariffs_title_text),
+                style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = "Add post"
-                )
-            }
-            OutlinedButton(
+                    .padding(start = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            MenuButton(
+                text = stringResource(id = R.string.app_tariffs_list_screen_name),
                 onClick = {
                     navigator.navigate(TariffsScreenDestination, onlyIfResumed = true)
-                },
+                }
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.menu_details_title_text),
+                style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = "Tariffs"
-                )
-            }
+                    .padding(start = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            MenuButton(
+                text = stringResource(id = R.string.menu_about_app_button_text),
+                isEnable = false,
+                onClick = {
+
+                }
+            )
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun PreviewHomeScreen() {
-//    HomeScreen()
+private fun MenuButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    isEnable: Boolean = true,
+    onClick: () -> Unit,
+    horizontalPadding: Dp = 16.dp,
+    verticalPadding: Dp = 16.dp,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium
+) {
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .clickable(enabled = isEnable, onClick = onClick)
+                .padding(horizontal = horizontalPadding, vertical = verticalPadding)
+        ) {
+            Text(
+                text = text,
+                style = textStyle
+            )
+        }
+        Divider(modifier = Modifier.fillMaxWidth())
+    }
 }
