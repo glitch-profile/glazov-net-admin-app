ed3fd133ebebbeff3cff6704d9bae20f074b3dab
Ivan <korablev2002@yandex.ru>
2023-10-14 23:11:17 +0500

    Added icons parameters for MenuButton composable

--- app/src/main/java/com/example/glazovnetadminapp/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/glazovnetadminapp/presentation/HomeScreen.kt
@@ -3,14 +3,22 @@ package com.example.glazovnetadminapp.presentation
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -19,6 +27,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,13 +75,15 @@ fun HomeScreen(
                 text = stringResource(id = R.string.app_posts_screen_name),
                 onClick = {
                     navigator.navigate(PostsScreenDestination, onlyIfResumed = true)
-                }
+                },
+                iconVector = Icons.Default.DateRange
             )
             MenuButton(
                 text = stringResource(id = R.string.app_add_post_screen_name),
                 onClick = {
                     navigator.navigate(AddPostScreenDestination, onlyIfResumed = true)
-                }
+                },
+                iconVector = Icons.Default.Add
             )
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -86,7 +98,8 @@ fun HomeScreen(
                 text = stringResource(id = R.string.app_tariffs_list_screen_name),
                 onClick = {
                     navigator.navigate(TariffsScreenDestination, onlyIfResumed = true)
-                }
+                },
+                iconVector = Icons.Default.List
             )
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -102,7 +115,8 @@ fun HomeScreen(
                 isEnable = false,
                 onClick = {
 
-                }
+                },
+                iconVector = Icons.Default.Info
             )
         }
     }
@@ -114,17 +128,28 @@ private fun MenuButton(
     text: String,
     isEnable: Boolean = true,
     onClick: () -> Unit,
+    iconVector: ImageVector? = null,
+    iconDescription: String = "",
+    iconTint: Color = MaterialTheme.colorScheme.primary,
     horizontalPadding: Dp = 16.dp,
     verticalPadding: Dp = 16.dp,
     textStyle: TextStyle = MaterialTheme.typography.bodyMedium
 ) {
     Column {
-        Box(
+        Row(
             modifier = modifier
                 .fillMaxWidth()
                 .clickable(enabled = isEnable, onClick = onClick)
                 .padding(horizontal = horizontalPadding, vertical = verticalPadding)
         ) {
+            if (iconVector !== null) {
+                Icon(
+                    imageVector = iconVector,
+                    contentDescription = iconDescription,
+                    tint = iconTint
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+            }
             Text(
                 text = text,
                 style = textStyle
