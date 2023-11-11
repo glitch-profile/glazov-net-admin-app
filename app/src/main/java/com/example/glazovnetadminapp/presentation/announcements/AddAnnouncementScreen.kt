211d815aee4eaddb49459e7ef234df0a1562145a
Ivan <korablev2002@yandex.ru>
2023-11-11 14:08:16 +0500

    added filters screen to add announcement screen

--- app/src/main/java/com/example/glazovnetadminapp/presentation/announcements/AddAnnouncementScreen.kt
+++ app/src/main/java/com/example/glazovnetadminapp/presentation/announcements/AddAnnouncementScreen.kt
@@ -1,15 +1,19 @@
 package com.example.glazovnetadminapp.presentation.announcements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,15 +25,19 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.glazovnetadminapp.R
+import com.example.glazovnetadminapp.domain.models.announcements.AddressFilterElement
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,7 +51,9 @@ fun AddAnnouncementScreen(
     var text by remember {
         mutableStateOf("")
     }
-    val filters = viewModel.filtersToEdit.collectAsState()
+    var filters by rememberSaveable {
+        mutableStateOf(emptyList<AddressFilterElement>())
+    }
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(
@@ -115,6 +125,36 @@ fun AddAnnouncementScreen(
                     )
                 }
             )
+            Spacer(modifier = Modifier.height(4.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                filters.forEach { filter ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            text = "${filter.city}, ${filter.street}, ${filter.houseNumber}"
+                        )
+                        IconButton(
+                            onClick = { /*TODO*/ }
+                        ) {
+                           Icon(
+                               modifier = Modifier
+                                   .size(25.dp),
+                               imageVector = Icons.Default.Clear,
+                               contentDescription = "remove filter"
+                           )
+                        }
+                    }
+                }
+            }
         }
     }
 }
